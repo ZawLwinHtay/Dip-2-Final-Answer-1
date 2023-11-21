a5b7e7afddf82c9da56babe046ad601e3382fad8
Zaw Lwin Htay <zawwlwinhtay789@gmail.com>
2023-11-21 16:39:46 +0630

    working 4 Fill Amount

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import ast
 import threading
 import socket
 import json
@@ -11,6 +12,10 @@ class Bids_client:
         self.username = 'username'
         self.status = True
 
+        self.myId = None
+        self.myInfo = []
+        self.updateInfo = []
+
     def start_auction(self):
         while True:
             try:
@@ -19,7 +24,7 @@ class Bids_client:
                 if opt == 1:
                     self.register()
                 elif opt == 2:
-                    print('2', opt)
+                    self.login()
                 elif opt == 3:
                     exit(1)
                 else:
@@ -30,18 +35,91 @@ class Bids_client:
 
     # register page
     def register(self):
-        name = input("Enter Your Name for Registration: ")
-        password = input("Enter Password for Registration: ")
-        email = input("Enter Email for Register: ")
-        phone = input("Enter Phone for Register: ")
+        name = input("Enter 'Your Name' for Registration: ")
+        password = input("Enter 'Password' for Registration: ")
+        email = input("Enter 'Email' for Registration: ")
+        phone = input("Enter 'Phone' for Registration: ")
         show_money = "0"
 
-        # send_data = {"msg": "register", "username": name, "password": password}
-        # send_data = {"register": {"username": "Zaw", "password": "Zaw", "email": "Zaws@gmail.com"}}
         send_data = "register" + "~" + name + "~" + password + "~" + email + "~" + phone + "~" + show_money
         self.client.send(send_data.encode('utf-8'))
         recv_data = self.client.recv(1024).decode('utf-8')
         print(recv_data)
+
+    def login(self):
+        email = input("Enter 'Email' for Login: ")
+        password = input("Enter 'Password' for Login: ")
+        send_data = "login" + "~" + email + "~" + password
+        self.client.send(send_data.encode('utf-8'))
+        recv_data = self.client.recv(1024).decode('utf-8')
+        recv_data = ast.literal_eval(recv_data)
+        if recv_data[0]:
+            self.myInfo = recv_data[1]
+            self.myId = recv_data[2]
+            self.hello_boss()
+        else:
+            print(f">>> Login Error Msg: {recv_data[1]}")
+
+    def hello_boss(self):
+        while True:
+            try:
+                print("===== This is Home Page ===== ")
+                print(f"===== Wellcome <<< {self.myInfo['username']} >>> :id < {self.myId} > =====")
+                print(f"+++++ Your Show Money: {self.myInfo['show_money']} kyats +++++")
+                self.updateInfo = self.myInfo.copy()
+                opt = int(input("Press 1 to Create Auction:\nPress 2 to Go to Place Bids:\nPress 3 to Transfer Amount:\n"
+                                "Press 4 to Fill Amount:\nPress 5 Show All Auctions Status:\nPress 6 to Show My Auctions and Status:\n"
+                                "Press 7 to Logout:\nPress 8 to Exit this app:\nEnter Something: "))
+                if opt == 1:
+                    self.client.send("Press 1 to Create Auction".encode('utf-8'))
+                    msg = self.client.recv(1024).decode('utf-8')
+                elif opt == 2:
+                    self.client.send("Press 2 to Go to Place Bids".encode('utf-8'))
+                    msg = self.client.recv(1024).decode('utf-8')
+                elif opt == 3:
+                    self.client.send("Press 3 to Transfer Amount".encode('utf-8'))
+                    msg = self.client.recv(1024).decode('utf-8')
+                elif opt == 4:
+                    self.fill_amount()
+                elif opt == 5:
+                    self.client.send("Press 5 Show All Auctions Status".encode('utf-8'))
+                    msg = self.client.recv(1024).decode('utf-8')
+                elif opt == 6:
+                    self.client.send("Press 6 to Show My Auctions and Status".encode('utf-8'))
+                    msg = self.client.recv(1024).decode('utf-8')
+                elif opt == 7:
+                    self.client.send("exit".encode('utf-8'))
+                    msg = self.client.recv(1024).decode('utf-8')
+                    print("Server: ", msg)
+                    self.myInfo = []
+                    self.start_auction()
+                elif opt == 8:
+                    self.client.send("exit".encode('utf-8'))
+                    msg = self.client.recv(1024).decode('utf-8')
+                    print("Server: ", msg)
+                    exit(1)
+
+            except Exception as err:
+                print("hello_boss err: ", err)
+                self.hello_boss()
+
+    # hello boss press 4 Fill Amount
+    def fill_amount(self):
+        try:
+            print("===== Fill Amount Page =====")
+            amount = int(input("Enter Amount You want to fill: "))
+            self.updateInfo["show_money"] += amount
+            data = ["fill_amount", {f"{self.myId}": self.myInfo}, {f"{self.myId}": self.updateInfo}]
+            self.client.send(f"{data}".encode('utf-8'))
+            msg = self.client.recv(1024).decode('utf-8')
+            if msg:
+                print("Server: ", msg)
+                self.myInfo = self.updateInfo
+            else:
+                print("Update User Not Found!")
+        except Exception as err:
+            print("fill_amount_err: ", err)
+            self.fill_amount()
 
     # def client_receive(self):
     #     while True:
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import ast
 import json
 import threading
 import socket
@@ -12,7 +13,6 @@ class Bids_server:
         self.server.listen()
         self.clients: list = []
         self.bids: list = []
-        # self.aliases: list = []
 
         # files
         self.users_txt = "users.txt"
@@ -23,47 +23,79 @@ class Bids_server:
         self.all_user = []
 
     def broadcast(self, message):
-        # print(self.clients)
         for client in self.clients:
             client.send(message)
-            # print("done")
 
     def handle_client(self, client, addr):
         while True:
             try:
                 message = client.recv(1024)
                 data = message.decode('utf-8').split('~')
-                # client.send(f"Data recv: {data}\n{addr}".encode('utf-8'))
 
                 goTo = data[0]
                 if goTo == "register":
                     self.register(client, goTo, data)
+                elif goTo == "login":
+                    self.login(client, goTo, data)
 
             except:
                 self.clients.remove(client)
                 client.close()
-                print(f'{addr} has disconnected form this app.!')
+                print(f'{addr} has disconnected form this app!')
                 break
 
     def register(self, client, log, data):
         user_found = self.check_user(log, data)
         if not user_found:
-            # print("check", user_found)
             add_user = self.writeFile(self.users_txt, data)
             self.readFile(self.users_txt)
             client.send(f">>> {add_user}: {data[1]}".encode('utf-8'))
         else:
             client.send(f">>> Error Msg for Register: {user_found}".encode('utf-8'))
 
+    def login(self, client, log, data):
+        user_found = self.check_user(log, data)
+        print("login user found: ", user_found)
+        client.send(f"{user_found}".encode('utf-8'))
+        if user_found[0]:
+            while True:
+                msg = client.recv(1024).decode('utf-8')
+                if msg:
+                    if msg == "exit":
+                        client.send("See You Again!".encode('utf-8'))
+                        break
+                    else:
+                        try:
+                            msg = ast.literal_eval(msg)
+                            if msg[0] == "fill_amount":
+                                update = self.update_user_data(msg[1], msg[2])
+                                client.send(f"{update}".encode('utf-8'))
+                        except Exception as err:
+                            print("This is error", err)
+                            continue
+                else:
+                    break
+
+    def update_user_data(self, oldData: dict, newData: dict):
+        exist = 0
+        for i in range(len(self.all_user)):
+            if self.all_user[i] == oldData:
+                self.all_user[i] = newData
+                exist = 1
+                break
+        # over wirte (update user data)
+        with open(self.users_txt, 'w') as file:
+            for user in self.all_user:
+                file.write(f"{json.dumps(user)}\n")
+        self.readFile(self.users_txt)
+        return exist
+
     def check_user(self, log, data):
         exits = None
         if log == "register":
-            # print("check_user: ", data)
             for i in range(len(self.all_user)):
                 check_name = self.all_user[i][f'{i}']['username']
                 check_email = self.all_user[i][f'{i}']['email']
-                # print(check_name, data[1])
-                # print(check_email, data[3])
                 if check_name == data[1] and check_email == data[3]:
                     exits = "username and email are already exits, Please, Try again with other different username and email!"
                     break
@@ -75,15 +107,27 @@ class Bids_server:
                     break
                 else:
                     exits = 0
-            # print("exits: ", exits)
             return exits
+        elif log == "login":
+            user = None
+            for i in range(len(self.all_user)):
+                check_email = self.all_user[i][f'{i}']['email']
+                check_pass = self.all_user[i][f'{i}']['password']
+                if check_email == data[1] and check_pass == data[2]:
+                    user = [1, self.all_user[i][f'{i}'], i]
+                    print(user)
+                    break
+                else:
+                    user = [0, "Email or Password is not found. Please, Try again!!!"]
+            return user
 
     # read file
     def readFile(self, filename):
         self.all_user = []
         with open(filename, "r") as file:
             for line in file.readlines():
-                user = json.loads(line.strip())
+                line = line.strip()
+                user = json.loads(line)
                 self.all_user.append(user)
 
     # write file
@@ -92,7 +136,7 @@ class Bids_server:
         with open(filename, "r") as rfile:
             with open(filename, "a") as file:
                 ids = list(json.loads(rfile.readlines()[-1]).keys())
-                data = {"username": data[1], "password": data[2], "email": data[3], "phone": data[4], "show_money": int(data[5])}
+                data = {"username": data[1], "password": data[2], "email": data[3], "phone": data[4], "show_money": int(data[5]), "my_auctions": []}
                 data = {int(ids[0]) + 1: data}
                 print(data)
                 file.write(json.dumps(data) + "\n")
